a55634af3a624406b77c52a4b191c4ea6cec2a3d
Elisa <3bperezelisa@gmail.com>
2024-01-30 08:59:04 +0100

    first commit, config

---
+++ ruralHack/src/main/java/com/ruralHack/RuralHackApplication.java
@@ -0,0 +1,13 @@
+package com.ruralHack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuralHackApplication.class, args);
+	}
+
+}
---
+++ ruralHack/src/test/java/com/ruralHack/RuralHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruralHack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralHackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
